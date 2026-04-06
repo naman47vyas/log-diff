9c67e7a83b1215de471ca5814431946e1bc23adc
naman47vyas <naman.vyas@middleware.io>
2026-04-06 15:33:25 +0530

    driver code for parser in main

--- cmd/logdiff/main.go
+++ cmd/logdiff/main.go
@@ -1,26 +1,44 @@
 package main
 
-func main() {
-	// preFile := flag.String("pre", "", "path to the pre-release file")
-	// postFile := flag.String("post", "", "path to the post-release file")
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
-	// format := flag.String("format", "bracket", "log format: bracket")
+	"github.com/k0kubun/pp"
+	"github.com/naman47vyas/log-diff/internal/parser"
+)
+
+func main() {
+	preFile := flag.String("pre", "", "path to the pre-release file")
+	postFile := flag.String("post", "", "path to the post-release file")
+
+	format := flag.String("format", "bracket", "log format: bracket")
 	// simTh := flag.Float64("sim-threshold", 0.4, "Drain similarity threshold (0.0-1.0)")
 
 	// freqTh := flag.Float64("freq-threshold", 2.0, "Frequency change ratio to flag as changed")
 
-	// if *preFile == "" || *postFile == "" {
-	// 	fmt.Fprintln(os.Stderr, "usage: logdiff --pre <file> --post <file>")
-	// 	flag.PrintDefaults()
-	// 	os.Exit(1)
-	// }
+	if *preFile == "" || *postFile == "" {
+		fmt.Fprintln(os.Stderr, "usage: logdiff --pre <file> --post <file>")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-	// p, err :=  newParser(*format)
+	p, err := newParser(*format)
 
-	// if err != nil  {
-	// 	log.Fatalf("invalid format: %v", err)
-	// }
+	pp.Println(p)
+	if err != nil {
+		log.Fatalf("invalid format: %v", err)
+	}
 
-	// norm := normalizer.new()
+}
 
+func newParser(format string) (parser.Parser, error) {
+	switch format {
+	case "bracket":
+		return parser.NewBracketParser(), nil
+	default:
+		return nil, fmt.Errorf("unsupported format: %s", format)
+	}
 }
